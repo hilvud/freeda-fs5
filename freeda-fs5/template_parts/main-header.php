e0fbaf1c1bc3186d8a423d3a9b8379b82b947ce1
hilvud <teslabarinstorm@gmail.com>
2024-04-24 15:37:29 +0200

    prebacivanje na git posle prvog casa

---
+++ freeda-fs5/template_parts/main-header.php
@@ -0,0 +1,8 @@
+<!--main header-->
+<header class="main-header">
+    <hgroup>
+        <h1>VREME JE ZA KAFICU</h1>
+        <p>Prva pauza na poslu je rezervisana za kaficu</p>
+        <a href="" class="btn btn-yellow">Saznaj vise/<a>
+    </hgroup>
+</header>
